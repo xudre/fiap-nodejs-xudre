68096e38e1a599d53e3cfea4f0e4d6af2a2f1abe
Pedro Xudre <pedro@xudre.com>
2020-04-25 16:42:39 -0300

    Token acquisition simplified.

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,17 +2,13 @@ const jwt = require('jsonwebtoken');
 const ApiError = require('../class/ApiError');
 
 const verifyToken = (req, res, next) => {
-    const auth = req.headers.authorization;
+    const auth = /^JWT (.+)$/i.exec(req.headers.authorization);
 
     if (!auth) {
         return res.status(401).send(new ApiError('Token não encontrado', 'token_not_found'));
     }
 
-    const token = req.headers.authorization.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).send(new ApiError('Token não encontrado', 'token_not_found'));
-    }
+    const token = auth[1];
 
     jwt.verify(token, process.env.SECRET, (error, decode) => {
         if (error) {
